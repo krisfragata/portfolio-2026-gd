39d7bf804ffa67124f3fe8b9af69a3b0eb9ce494
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2026-04-15 14:15:36 -0400

    feat(about):add all contents pertaining to about section includes adding new function renderWorkInfo to render employment information from works_list file migrating work_list.ts and education_list.ts to /about route adds new css classes: italics and simplifies details in education and work history

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import { Education, educationList } from "./education_list";
+import { Work, works } from "./work_list";
 
 const About: React.FC = () => {
+    /**
+     * @description renders education list items
+     * @returns JSX component
+     */
     const renderEducationInfo = () => {
             return educationList.map((edu:Education, index) => {
             return (<div className="education-item" key={index}>
                         <div className="about-name">{edu.schoolName}</div>
+                        <div className="details italics">{edu.description}</div>
                         <div className="details">{edu.years}</div>
-                        {/* <div className="description">{edu.description}</div> */}
                     </div>);
         });
     }
+
+    /**
+     * @description renders work list items
+     * @returns JSX component
+     */
+    const renderWorkInfo = () => {
+        return works.map((work, index) => {
+            return (
+                <div className="work-item" key={work.id}> 
+                    <div className="about-name">{work.companyName}</div>
+                    <div className="details italics">{work.position}</div>
+                    <div className="details">{work.years}</div>
+                </div>
+            )
+        })
+    }
+    
     return (
         <div className="about-container">
             <div className="section">
@@ -18,7 +40,7 @@ const About: React.FC = () => {
                     About Me
                 </div>
                 <p className="summary">
-                    Enjoyer of music, literature, climbing. Skilled in photography, film, software engineering, design, music making.
+                    Enjoyer of music, literature, climbing. Skilled in photography, film, software engineering, design, music, and teaching cats tricks (mainly of the high-five variation).
                 </p>
             </div>
             <div className="section">
@@ -27,6 +49,14 @@ const About: React.FC = () => {
                 </div>
                 <div className="education">
                     {renderEducationInfo()}
+                </div>
+            </div>
+            <div className="section">
+                <div className="header">
+                    Employment
+                </div>
+                <div className="education">
+                    {renderWorkInfo()}
                 </div>
             </div>
         </div>
