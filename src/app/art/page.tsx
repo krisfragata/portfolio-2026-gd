d08ad62498c5802b97a34d7486d4ed7ba4104cd1
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2026-04-16 15:30:01 -0400

    feat(art):add art modal window this commit adds the following functions to aid in rendering the art info modals: renderArtList, renderInfoModal

--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import { Art, artList } from "./art_list";
+import ArtInfoModal from "@/components/ArtInfoModal";
 
 const ArtProjects: React.FC = () => {
+    // track state of info modal
+    const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+    const [openProject, setOpenProject] = useState<Art>({
+        artwork: '',
+        description: [],
+        id: '0',
+        imageSources: [],
+        links: [],
+        type: '',
+        years: '',
+    })
+
     /**
      * @description renders art list items
      * @returns JSX component
@@ -9,15 +23,42 @@ const ArtProjects: React.FC = () => {
     const renderArtList = () => {
         return artList.map((art:Art, index) => {
             return (
-                <div className="art-item" key={art.id}>
-                   {art.artwork} - <span className="italics">{art.details}</span> 
+                <div 
+                    className="art-item"
+                    key={art.id}
+                    onClick={() => {
+                        const isOpen = isInfoModalOpen;
+                        setIsInfoModalOpen(!isOpen);
+                        setOpenProject(art);
+                    }}
+                >
+                   {art.artwork} - <span className="italics">{art.type}</span> 
                 </div>
             );
         })
     }
+
+    // create method to close all info modals
+    // create method to open specific info modal
+
+    /**
+     * @description render info modal
+     * @returns JSX component
+     */
+    const renderInfoModal = () => {
+        return (
+            <ArtInfoModal 
+                project={openProject}
+                setIsModalOpen={setIsInfoModalOpen}
+                isModalOpen={isInfoModalOpen}
+                extraStyling=""
+            />
+        )
+    }
     return (
         <div className="art-container">
             {renderArtList()}
+            {renderInfoModal()}
         </div>
     );
 };
