69b9ca75a159c9f564e16a5c13e59f1ee8488aac
Kristinne Fragata <krisfragata@Kristinnes-MacBook-Air.local>
2026-04-17 09:58:50 -0400

    feat(reading):add new reading page and reading_list

---
+++ src/app/reading/reading_list.ts
@@ -0,0 +1,43 @@
+export interface Book {
+    title: string;
+    author: string;
+    personalRating: string;
+}
+
+export const bookList:Book[] = [
+    {
+        title: 'North Woods',
+        author: 'Daniel Mason',
+        personalRating: '4.5'
+    },
+    {
+        title: 'Trust',
+        author: 'Hernan Diaz',
+        personalRating: '4.5'
+    },
+    {
+        title: 'The Alchemist',
+        author: 'Paulo Coelho',
+        personalRating: '3.25'
+    },
+    {
+        title: 'Babel',
+        author: 'R.F Kuang',
+        personalRating: '4.25'
+    },
+    {
+        title: 'The Night Circus',
+        author: 'Erin Morgenstern',
+        personalRating: '4'
+    },
+    {
+        title: 'Exit West',
+        author: 'Mohsin Hamid',
+        personalRating: '4'
+    },
+    {
+        title: 'A Tale for the Time Being',
+        author: 'Ruth Ozeki',
+        personalRating: '4'
+    },
+]
